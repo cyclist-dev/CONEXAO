1667a1fe3779ef67c02a61152c33ccd57b67898d
cyclist-dev <moc.profissional@gmail.com>
2026-05-05 21:55:48 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$host="localhost";
+$banco="biblioteca";
+$usuario="root";
+$senha="";
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8",$usuario,$senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    die("Erro ao conectar: ".$e->getMessage());
+}
+
+?>
